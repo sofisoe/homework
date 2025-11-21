529adb2375f46fbab1d1c8cd427beada00d10d60
Sofasluci <slsofa@icloud.com>
2025-11-22 01:53:09 +0300

    arr(letters)

--- lesson8.cpp
+++ lesson8.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
-    int n;
     int arr[100];
-    cout << "Enter the size"<<endl;
-    cin>>n;
-    for (int i=0; i < n; i++) {
-        arr[i]=n-(i+1);
-        cout<<arr[i]<<" ";
+    int i;
+    char l;
+    string w;
+
+    cout<<"type a word"<<endl;
+    getline(cin, w);
+    
+    int s = w.size();
+    cout << "shifr: "<<endl;
+
+    for (int i=0; i<s; i++) {
+        char l = w[i];
+        if (l >='a' && l<= 'z') {
+            arr[i]=(l-'a'+1);
+    }
+        else {
+            cout<<"English little letters please";
+            arr[i]=0;
+        }
+    }
+    for (int i=0; i<s; i++) {
+        cout << arr[i]<<" ";
     }
 
+
 }
-
-
