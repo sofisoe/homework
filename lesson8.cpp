1314141f8c58754820eb788249add7a75b7302f2
Sofasluci <slsofa@icloud.com>
2025-11-22 00:29:58 +0300

    arr(vice versa)

--- lesson8.cpp
+++ lesson8.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main () {
     int n;
+    int arr[100];
     cout << "Enter the size"<<endl;
     cin>>n;
-    int array[100];
-    for (int i=0; i<n; i++) {
-        array[i]=i;
-        }
-    for (int i=0; i<n; i++) {
-        cout<< array[i]<< " ";
+    for (int i=0; i < n; i++) {
+        arr[i]=n-(i+1);
+        cout<<arr[i]<<" ";
     }
+
 }
+
+
